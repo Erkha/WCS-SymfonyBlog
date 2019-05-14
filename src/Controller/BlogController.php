1f3fdb2dcd70b298b2874426b991236ee3f36ac8
Erkha <jmtatout@gmail.com>
2019-05-14 12:58:46 +0200

    symfony7: filtrer avec Doctrine

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Article;
-
+use App\Entity\Category;
 
 class BlogController extends AbstractController
 {		
@@ -32,14 +32,90 @@ class BlogController extends AbstractController
       );
     }
 
-  /**
-   * @Route("/blog/show/{slug}", requirements={"slug"="[a-z\-0-9]+"}, name="blog_show")
-   */
-    public function show($slug ='Article Sans Titre')
-    {
-    	$slug = ucwords(str_replace('-', ' ', $slug));
-	    return $this->render('blog/show.html.twig', [
-	            'slug' => $slug
-	    ]);
+/**
+ * Getting an article with a formatted slug for title
+ *
+ * @param string $slug The slugger
+ *
+ * @Route("/blog/{slug<^[a-z0-9-]+$>}",
+ *     defaults={"slug" = null},
+ *     name="blog_show")
+ *  @return Response A response instance
+ */
+ public function show(?string $slug) : Response
+ {
+     if (!$slug) {
+            throw $this
+            ->createNotFoundException('No slug has been sent to find an article in article\'s table.');
+        }
+
+     $slug = preg_replace(
+      '/-/',
+      ' ', ucwords(trim(strip_tags($slug)), "-")
+        );
+
+     $article = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findOneBy(['title' => mb_strtolower($slug)]);
+
+     if (!$article) {
+          throw $this->createNotFoundException(
+          'No article with '.$slug.' title, found in article\'s table.'
+      );
     }
+
+     return $this->render(
+     'blog/show.html.twig',
+      [
+              'article' => $article,
+              'slug' => $slug,
+      ]
+    );
+}
+/**
+ * Getting a list of all articles from a slug category
+ *
+ * @param string $slug The slugger
+ *
+ * @Route("/category/{slug<^[a-z0-9-]+$>}",
+ *     defaults={"slug" = null},
+ *     name="show_category")
+ *  @return Response A response instance
+ */
+ public function showByCategory(?string $slug) : Response
+ {
+     if (!$slug) {
+            throw $this
+            ->createNotFoundException('No slug has been sent to find a category in category\'s table.');
+        }
+
+     $slug = preg_replace(
+      '/-/',
+      ' ', ucwords(trim(strip_tags($slug)), "-")
+        );
+
+     $category = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findOneBy(['name' => mb_strtolower($slug)]);
+
+     if (!$category) {
+          throw $this->createNotFoundException(
+          'No category with '.$slug.' name, found in category\'s table.'
+      );
+    }else
+    {
+       $articles = $this->getDoctrine()
+              ->getRepository(Article::class)
+              ->findby(['category'=>$category->getId()],
+                       ['id'=>'DESC'],3);
+       return $this->render(
+       'blog/category.html.twig',
+        [
+                'articles' => $articles,
+                'slug' => $slug,
+                'category' => $category
+        ]
+      );
+     }
+}
 }
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Controller\Article;
 
 
 class DefaultController extends AbstractController
